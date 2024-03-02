a6a854c6ed5cd52fa1b08f1cc68e8ffe8653dca4
NathanPERIER <periernathan2000@gmail.com>
2024-03-02 17:55:51 +0100

    Added method to format the result as JSON

---
+++ src/server/handlers/json_handler.ts
@@ -0,0 +1,17 @@
+import { RequestData } from '../data.js';
+
+
+function replacer(this: any, _key: string, value: any): any {
+    if(value instanceof Map) {
+        return Object.fromEntries(value.entries());
+    }
+    return value;
+}
+
+
+export function handle_json(req: RequestData): {lines: string[], content_type: string} {
+
+    const line = JSON.stringify(req, replacer, undefined);
+    
+	return { lines: [line], content_type: 'application/json' };
+}
